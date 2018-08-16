14df7056dd37c8837250e75c53a17b78dafa64db
Your Name <hbtanjun2012@126.com>
2018-08-16 14:40:35 +0800

    头条demo

---
+++ app/build/generated/source/buildConfig/release/me/weyye/todaynews/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.weyye.todaynews;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "me.weyye.todaynews";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 101;
+  public static final String VERSION_NAME = "1.0.1";
+}
